30524f2cd5f165c57f7586c61fe428a5da6c6c63
Chris Palmer <cgpalmer91@gmail.com>
2021-06-12 16:02:09 +0000

    Addign query for teams.!

--- home/admin.py
+++ home/admin.py
@@ -10,8 +10,19 @@ class PlayersAdmin(admin.ModelAdmin):
 
     ordering = ('current_score',)
 
+class TeamsAdmin(admin.ModelAdmin):
+    list_display = (
+        'name_of_person',
+        'team_1',
+        'team_2',
+        'team_3',
+    )
+
+    ordering = ('current_score',)
+
 
 
 
 
 admin.site.register(Players, PlayersAdmin)
+admin.site.register(Players, TeamsAdmin)
--- home/views.py
+++ home/views.py
@@ -4,7 +4,9 @@ from .models import Players
 # Create your views here.
 def index(request):
     players = Players.objects.all()
+    teams = Teams.objects.all()
     context = {
-        'players': players
+        'players': players,
+        'teams': teams
     }
     return render(request, 'home/index.html', context)
