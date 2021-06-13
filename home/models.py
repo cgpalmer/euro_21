413d878a0cbd6774836e9ddbd669b4b628328db6
Chris Palmer <cgpalmer91@gmail.com>
2021-06-13 17:43:08 +0000

    Adding in more awards

--- home/models.py
+++ home/models.py
@@ -6,6 +6,8 @@ from django.db import models
 class Players(models.Model):
     name_of_person = models.CharField(max_length=254, null=True, blank=True)
     current_score = models.IntegerField(null=True, blank=True)
+    total_goals_for = models.IntegerField(null=True, blank=True)
+    total_goals_against = models.IntegerField(null=True, blank=True)
    
     def __str__(self):
         return self.name_of_person
--- home/static/js/main.js
+++ home/static/js/main.js
@@ -12,4 +12,22 @@ const names_of_players = ["Joe", "Maddie", "Chris", "Shannon", "Tori", "Jack", "
         console.log(names_of_players[i]);
     }
 
+    for (i = 0; i < names_of_players.length; i++) {
+        console.log(`${names_of_players[i]}_ga`);
+        let score_value = $(`#${names_of_players[i]}_ga`).val();
+      
+        console.log(score_value)
+        $(`.${names_of_players[i]}_ga`).width(`${score_value}%`);
+        console.log(names_of_players[i]);
+    }
+
+    for (i = 0; i < names_of_players.length; i++) {
+        console.log(`${names_of_players[i]}_gf`);
+        let score_value = $(`#${names_of_players[i]}_gf`).val();
+      
+        console.log(score_value)
+        $(`.${names_of_players[i]}_gf`).width(`${score_value}%`);
+        console.log(names_of_players[i]);
+    }
+
 });
